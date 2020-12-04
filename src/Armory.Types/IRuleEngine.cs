ee55d312b96eb925dfeced38ae5b93274f57676e
Johnathon Mohr <johnmohr@microsoft.com>
2020-12-04 11:58:22 -0800

    Adding interface and new input types for running the engine

--- src/Armory.Types/IRuleEngine.cs
+++ src/Armory.Types/IRuleEngine.cs
@@ -1,10 +1,18 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+
 namespace Armory.Types
 {
     public interface IRuleEngine
     {
-
+        /// <summary>
+        /// Evaluates a template using the specified rules.
+        /// </summary>
+        /// <param name="templateContext">The template context to evaluate.</param>
+        /// <param name="ruleDefinitions">The rules to evaluate the template with.</param>
+        /// <returns>The results of the rules against the template.</returns>
+        public IEnumerable<IResult> Run(TemplateContext templateContext, string ruleDefinitions);
     }
 }
