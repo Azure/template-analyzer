e766b0c5abba8208b9da356665a26751026a2ba6
Yanelis Lopez <yalope@microsoft.com>
2020-12-15 12:03:57 -0800

    Add comments to LeafExpressionTests

--- src/Armory.JsonRuleEngine.UnitTests/LeafExpressionTests.cs
+++ src/Armory.JsonRuleEngine.UnitTests/LeafExpressionTests.cs
@@ -37,8 +37,9 @@ namespace Armory.JsonRuleEngine.UnitTests
         [DataRow(null, "", false, singleField, null, 1, DisplayName = "No resource type, empty path, operator evaluates to false")]
         [DataRow(null, "some.path", true, singleField, null, 1, DisplayName = "No resource type, valid path, operator evaluates to true")]
         [DataRow("Microsoft.ResourceProvider/type", "properties.some.path", true, singleResource, "resources[0].properties.some.path", 0, DisplayName = "Resource Type specified with single matching resource, valid path, operator evaluates to true")]
-        public void Evaluate_ValidScope_ReturnsResultsOfOperatorEvaluation(string resourceType, string path, bool evaluationResult, string jtoken, string fullPathForEvaluateExpression, int timesResolveIsCalledOnOriginalObject)
+        public void Evaluate_ValidScope_ReturnsResultsOfOperatorEvaluation(string resourceType, string path, bool expectedEvaluationResult, string jtoken, string absolutePathForEvaluateExpression, int timesResolveIsCalledOnOriginalObject)
         {
+            // Arrange
             var ruleDefinition = new RuleDefinition
             {
                 Name = "testRule",
@@ -47,36 +48,50 @@ namespace Armory.JsonRuleEngine.UnitTests
                 HelpUri = "https://helpUri"
             };
 
+            // JObject to evaluate, in this test, this is a subset of an ARM template
             var jsonToEvaluate = JObject.Parse(jtoken);
+
+            // Setting up the Mock JsonPathResolver to return the expected values when JToken and Resolve are called
             var mockJsonPathResolver = new Mock<IJsonPathResolver>();
+            // The JToken property should return the JObject to evaluate
             mockJsonPathResolver
                 .Setup(s => s.JToken)
                 .Returns(jsonToEvaluate);
+            // Resolve for the provided json path should return the JsonPathResolver
             mockJsonPathResolver
                 .Setup(s => s.Resolve(It.Is<string>(p => string.Equals(p, path))))
                 .Returns(() => new[] { mockJsonPathResolver.Object });
 
-            var jTokenExpectedInEvaluateExpression = GetJToken(jsonToEvaluate, fullPathForEvaluateExpression);
+            // Call a helper function to get the appropriate scope of the JObject
+            // absolutePathForEvaluateExpression is null when resourceType is not defined
+            // When resourceType is defined, absolutePathForEvaluateExpression represents the absolute
+            // json path of the path parameter specified
+            var jTokenExpectedInEvaluateExpression = GetRelevantJTokenScope(jsonToEvaluate, absolutePathForEvaluateExpression);
 
+            // EvaluateExpression for the provided scope should return the expected evaluationResult
             var mockLeafExpressionOperator = new Mock<LeafExpressionOperator>();
             mockLeafExpressionOperator
                 .Setup(o => o.EvaluateExpression(It.Is<JToken>(token => token == jTokenExpectedInEvaluateExpression)))
-                .Returns(evaluationResult);
+                .Returns(expectedEvaluationResult);
 
             var leafExpression = new LeafExpression(ruleDefinition, resourceType, path, mockLeafExpressionOperator.Object);
+
+            // Act
             var results = leafExpression.Evaluate(jsonScope: mockJsonPathResolver.Object).ToList();
 
+            // Assert
+            // Verify the number of time Resolve, JToken, and EvaluateExpression were called
             mockJsonPathResolver.Verify(s => s.Resolve(It.Is<string>(p => string.Equals(p, path))), Times.Exactly(timesResolveIsCalledOnOriginalObject));
             mockJsonPathResolver.Verify(s => s.JToken, Times.Once);
 
             mockLeafExpressionOperator.Verify(o => o.EvaluateExpression(It.Is<JToken>(token => token == jTokenExpectedInEvaluateExpression)), Times.Once);
 
             Assert.AreEqual(1, results.Count);
-            Assert.AreEqual(evaluationResult, results.First().Passed);
+            Assert.AreEqual(expectedEvaluationResult, results.First().Passed);
             Assert.AreEqual(ruleDefinition, results.First().RuleDefinition);
         }
 
-        private JToken GetJToken(JToken jToken, string path)
+        private JToken GetRelevantJTokenScope(JToken jToken, string path)
         {
             if (path == null)
             {
