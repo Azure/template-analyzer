d9ba1d0368f63a964131169485eb03c45ccf3679
Yanelis Lopez <yalope@microsoft.com>
2020-12-16 15:20:35 -0800

    Simplify DataRow input for ArmoryTests

--- src/Armory.Core.UnitTests/ArmoryTests.cs
+++ src/Armory.Core.UnitTests/ArmoryTests.cs
@@ -11,65 +11,36 @@ namespace Armory.Core.UnitTests
     public class ArmoryTests
     {
         [DataTestMethod]
-        [DataRow(@"{
-                  ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
-                  ""contentVersion"": ""1.0.0.0"",
-                  ""resources"": [
-                    {
-                      ""apiVersion"": ""2018-02-01"",
-                      ""name"": ""resourceName"",
-                      ""type"": ""Microsoft.ServiceFabric/clusters"",
-                      ""properties"": {
-                        ""azureActiveDirectory"": {
-                          ""tenantId"": ""tenantId""
-                        }
-                      }
-                    }
-                  ]
-                }
-                ", 1, true, DisplayName = "Matching Resource with one passing result")]
-        [DataRow(@"{
-                  ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
-                  ""contentVersion"": ""1.0.0.0"",
-                  ""resources"": [
-                    {
-                      ""apiVersion"": ""2018-02-01"",
-                      ""name"": ""resourceName"",
-                      ""type"": ""Microsoft.ServiceFabric/clusters"",
-                      ""properties"": {
-                        ""azureActiveDirectory"": {
-                          ""someProperty"": ""propertyValue""
-                        }
-                      }
-                    }
-                  ]
-                }
-                ", 1, false, DisplayName = "Matching Resource with one failing result")]
-        [DataRow(@"{
-                  ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
-                  ""contentVersion"": ""1.0.0.0"",
-                  ""resources"": [
-                    {
-                      ""apiVersion"": ""2018-02-01"",
-                      ""name"": ""resourceName"",
-                      ""type"": ""Microsoft.Storage/storageAccountss"",
-                      ""properties"": {
-                        ""property1"": {
-                          ""someProperty"": ""propertyValue""
-                        }
-                      }
-                    }
-                  ]
-                }
-                ", 0, false, DisplayName = "0 matching Resources with no results")]
-        public void EvaluateRulesAgainstTemplate_ValidInputValues_ReturnCorrectResults(string template, int expectedResultCount, bool expectedResult)
+        [DataRow(@"{ ""azureActiveDirectory"": { ""tenantId"": ""tenantId"" } }", 1, true, DisplayName = "Matching Resource with one passing result")]
+        [DataRow(@"{ ""azureActiveDirectory"": { ""someProperty"": ""propertyValue"" } }", 1, false, DisplayName = "Matching Resource with one failing result")]
+        [DataRow(@"{ ""property1"": { ""someProperty"": ""propertyValue"" } }", 0, false, DisplayName = "0 matching Resources with no results")]
+        public void EvaluateRulesAgainstTemplate_ValidInputValues_ReturnCorrectResults(string resourceProperties, int expectedResultCount, bool expectedResult)
         {
+            string template = GenerateTemplate(resourceProperties, expectedResultCount);
+
             var results = Armory.EvaluateRulesAgainstTemplate(template, null);
 
             Assert.AreEqual(expectedResultCount, results.Count());
 
             if (expectedResultCount > 0)
                 Assert.AreEqual(expectedResult, results.First().Passed);
+        }
+
+        private string GenerateTemplate(string resourceProperties, int expectedResultCount)
+        {
+            string resourceType = expectedResultCount > 0 ? "Microsoft.ServiceFabric/clusters" : "Microsoft.Storage/storageAccounts";
+
+            return string.Format(@"{{
+            ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
+            ""contentVersion"": ""1.0.0.0"",
+            ""resources"": [
+                    {{
+                      ""apiVersion"": ""2018-02-01"",
+                      ""name"": ""resourceName"",
+                      ""type"": ""{1}"",
+                      ""properties"": {0}
+                    }}
+            ]}}", resourceProperties, resourceType);
         }
 
         [TestMethod]
