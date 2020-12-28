d8e3e94d4bd033a18d73fa40e2293787fad31f71
Varun Sharma <varunsh@microsoft.com>
2020-12-27 20:37:07 -0800

    Move parser to separate file

---
+++ src/Armory.Cli/CommandLineParser.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Armory.Cli
+{
+    internal class CommandLineParser
+    {
+        private string[] _args;
+        RootCommand rootCommand;
+
+        public CommandLineParser(string[] args)
+        {
+            _args = args;
+
+            SetupCommandLineAPI();
+        }
+
+        public async Task InvokeCommandLineAPIAsync()
+        {
+            await rootCommand.InvokeAsync(_args).ConfigureAwait(false);
+        }
+
+        private RootCommand SetupCommandLineAPI()
+        {
+            // Command line API is setup using https://github.com/dotnet/command-line-api
+            // Create a root command 
+            rootCommand = new RootCommand();
+            rootCommand.Description = "ARMory - assesses Azure Resource Manager (ARM) Templates for security and best practice issues.";
+
+            // It has two commands - Assess-template and Assess-directory
+            rootCommand.AddCommand(SetupAssessTemplateCommand());
+
+            rootCommand.AddCommand(SetupAssessDirectoryCommand());
+            
+            return rootCommand;
+        }
+
+        private Command SetupAssessTemplateCommand()
+        {
+            // Setup assess-template 
+            Command assessTemplateCommand = new Command("assess-template");
+
+            Option<FileInfo> templateOption = new Option<FileInfo>(
+                    "--template-file-path",
+                    "The ARM template to assess")
+            {
+                IsRequired = true
+            };
+            templateOption.AddAlias("-t");
+
+            assessTemplateCommand.AddOption(templateOption);
+
+            Option<FileInfo> parameterOption = new Option<FileInfo>(
+                 "--parameters-file-path",
+                 "The parameter file to use when parsing the specified ARM template");
+            parameterOption.AddAlias("-p");
+
+            assessTemplateCommand.AddOption(parameterOption);
+
+            assessTemplateCommand.AddOption(SetupOutputFileOption());
+
+            assessTemplateCommand.Handler = CommandHandler.Create<FileInfo, FileInfo>((templateFilePath, parametersFilePath) =>
+            {
+                try
+                {
+                    Core.Armory armory = new Core.Armory(File.ReadAllText(templateFilePath.FullName), parametersFilePath == null ? null : File.ReadAllText(parametersFilePath.FullName));
+                    IEnumerable<Types.IResult> assessments = armory.EvaluateRulesAgainstTemplate();
+
+                    foreach (var assessment in assessments)
+                    {
+                        Console.WriteLine($"{assessment.RuleName}: {assessment.RuleDescription}, Result: {assessment.Passed.ToString()}");
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine($"An exception occured: {exp.Message}");
+                }
+
+            });
+
+            return assessTemplateCommand;
+        }
+
+        private Command SetupAssessDirectoryCommand()
+        {
+            // Setup assess-directory 
+            Command assessDirectoryCommand = new Command("assess-directory");
+
+            Option<DirectoryInfo> directoryOption = new Option<DirectoryInfo>(
+                    "--directory-path",
+                    "Directory to search for ARM templates (.json file extension)")
+            {
+                IsRequired = true
+            };
+            directoryOption.AddAlias("-d");
+
+            assessDirectoryCommand.AddOption(directoryOption);
+
+            Option<bool> recursiveOption = new Option<bool>(
+                    "--recursive",
+                    "Search directory and all subdirectories");
+            recursiveOption.AddAlias("-r");
+
+            assessDirectoryCommand.AddOption(recursiveOption);
+
+            assessDirectoryCommand.AddOption(SetupOutputFileOption());
+
+            assessDirectoryCommand.Handler = CommandHandler.Create<DirectoryInfo, bool>((directoryPath, recursive) =>
+            {
+                // TODO: This needs to call ARMory library and pass in a list of templates
+            });
+
+            return assessDirectoryCommand;
+        }
+
+        private Option SetupOutputFileOption()
+        {
+            Option<FileInfo> outputFileOption = new Option<FileInfo>(
+                        "--output-path",
+                        "Redirect output to specified file in JSON format");
+
+            outputFileOption.AddAlias("-o");
+
+            return outputFileOption;
+        }
+    }
+}
--- src/Armory.Cli/Program.cs
+++ src/Armory.Cli/Program.cs
@@ -1,118 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.CommandLine;
-using System.CommandLine.Invocation;
-using System.IO;
+﻿using System.Threading.Tasks;
 
 namespace Armory.Cli
 {
     class Program
     {
-        static int Main(string[] args)
+        static async Task Main(string[] args)
         {
-            return SetupCommandLineAPI().InvokeAsync(args).Result;
+            CommandLineParser commandLineParser = new CommandLineParser(args);
+
+            await commandLineParser.InvokeCommandLineAPIAsync().ConfigureAwait(false);
         }
 
-        private static RootCommand SetupCommandLineAPI()
-        {
-            // Create a root command 
-            var rootCommand = new RootCommand();
-            rootCommand.Description = "ARMory - assesses Azure Resource Manager (ARM) Templates for security and best practice issues.";
-
-            // It has two commands - Assess-template and Assess-directory
-            rootCommand.AddCommand(SetupAssessTemplateCommand());
-
-            rootCommand.AddCommand(SetupAssessDirectoryCommand());
-
-            return rootCommand;
-        }
-
-        private static Command SetupAssessTemplateCommand()
-        {
-            // Setup assess-template 
-            Command assessTemplateCommand = new Command("assess-template");
-
-            Option<FileInfo> templateOption = new Option<FileInfo>(
-                    "--template-file-path",
-                    "The ARM template to assess")
-            {
-                IsRequired = true
-            };
-            templateOption.AddAlias("-t");
-
-            assessTemplateCommand.AddOption(templateOption);
-
-            Option<FileInfo> parameterOption = new Option<FileInfo>(
-                 "--parameters-file-path",
-                 "The parameter file to use when parsing the specified ARM template");
-            parameterOption.AddAlias("-p");
-
-            assessTemplateCommand.AddOption(parameterOption);
-
-            assessTemplateCommand.AddOption(SetupOutputFileOption());
-
-            assessTemplateCommand.Handler = CommandHandler.Create<FileInfo, FileInfo>((templateFilePath, parametersFilePath) =>
-            {
-                //Console.WriteLine($"The value for --template-file-path is: {templateFilePath?.FullName ?? "null"}");
-                //Console.WriteLine($"The value for --parameters-file-path is: {parametersFilePath?.FullName ?? "null"}");
-
-                Core.Armory armory = new Core.Armory(templateFilePath.FullName);
-                IEnumerable<Types.IResult> assessments = armory.EvaluateRulesAgainstTemplate();
-
-                foreach (var assessment in assessments)
-                {
-                    Console.WriteLine($"{assessment.RuleName} at {assessment.LineNumber}");
-                }
-
-            });
-
-            return assessTemplateCommand;
-        }
-
-        private static Command SetupAssessDirectoryCommand()
-        {
-            // Setup assess-directory 
-            Command assessDirectoryCommand = new Command("assess-directory");
-
-            Option<DirectoryInfo> directoryOption = new Option<DirectoryInfo>(
-                    "--directory-path",
-                    "Directory to search for ARM templates (.json file extension)")
-            {
-                IsRequired = true
-            };
-            directoryOption.AddAlias("-d");
-
-            assessDirectoryCommand.AddOption(directoryOption);
-
-            Option<bool> recursiveOption = new Option<bool>(
-                    "--recursive",
-                    "Search directory and all subdirectories");
-            recursiveOption.AddAlias("-r");
-
-            assessDirectoryCommand.AddOption(recursiveOption);
-
-            assessDirectoryCommand.AddOption(SetupOutputFileOption());
-
-            assessDirectoryCommand.Handler = CommandHandler.Create<DirectoryInfo, bool>((directoryPath, recursive) =>
-            {
-                Console.WriteLine($"The value for --directory-path is: {directoryPath?.FullName ?? "null"}");
-                Console.WriteLine($"The value for --recursive is: {recursive}");
-
-                // TODO: This needs to call ARMory library
-            });
-
-            return assessDirectoryCommand;
-        }
-
-        private static Option SetupOutputFileOption()
-        {
-            Option<FileInfo> outputFileOption = new Option<FileInfo>(
-                        "--output-path",
-                        "Redirect output to specified file in JSON format");
-
-            outputFileOption.AddAlias("-o");
-
-            return outputFileOption;
-        }
     }
 }
