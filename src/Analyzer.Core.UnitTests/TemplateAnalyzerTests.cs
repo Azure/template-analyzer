8d4c3ea8d57bb35b844d79a1e85b6854e02a1907
Yanelis Lopez <yalope@microsoft.com>
2021-03-03 18:05:27 -0800

    Add test for multiple matching resources and getting correct results

--- src/Analyzer.Core.UnitTests/TemplateAnalyzerTests.cs
+++ src/Analyzer.Core.UnitTests/TemplateAnalyzerTests.cs
@@ -11,12 +11,15 @@ namespace Microsoft.Azure.Templates.Analyzer.Core.UnitTests
     public class TemplateAnalyzerTests
     {
         [DataTestMethod]
-        [DataRow(@"{ ""azureActiveDirectory"": { ""tenantId"": ""tenantId"" } }", "Microsoft.ServiceFabric/clusters", 1, true, DisplayName = "Matching Resource with one passing evaluation")]
-        [DataRow(@"{ ""azureActiveDirectory"": { ""someProperty"": ""propertyValue"" } }", "Microsoft.ServiceFabric/clusters", 1, false, DisplayName = "Matching Resource with one failing evaluation")]
+        [DataRow(@"{ ""azureActiveDirectory"": { ""tenantId"": ""tenantId"" } }", "Microsoft.ServiceFabric/clusters", 1, true, DisplayName = "1 matching Resource with 1 passing evaluation")]
+        [DataRow(@"{ ""azureActiveDirectory"": { ""someProperty"": ""propertyValue"" } }", "Microsoft.ServiceFabric/clusters", 1, false, DisplayName = "1 matching Resource with 1 failing evaluation")]
         [DataRow(@"{ ""property1"": { ""someProperty"": ""propertyValue"" } }", "Microsoft.Storage/storageAccounts", 0, false, DisplayName = "0 matching Resources with no results")]
-        public void EvaluateRulesAgainstTemplate_ValidInputValues_ReturnCorrectEvaluations(string resourceProperties, string resourceType, int expectedEvaluationCount, bool expectedEvaluationPassed)
+        [DataRow(@"{ ""azureActiveDirectory"": { ""tenantId"": ""tenantId"" } }", "Microsoft.ServiceFabric/clusters", 1, false, @"{ ""azureActiveDirectory"": { ""someProperty"": ""propertyValue"" } }", DisplayName = "2 matching Resources with 1 passing evaluation")]
+        public void EvaluateRulesAgainstTemplate_ValidInputValues_ReturnCorrectEvaluations(string resource1Properties, string resourceType, int expectedEvaluationCount, bool expectedEvaluationPassed, string resource2Properties = null)
         {
-            string template = GenerateTemplate(resourceProperties, resourceType);
+            // Arrange
+            string[] resourceProperties = { GenerateResource(resource1Properties, resourceType), GenerateResource(resource2Properties, resourceType) };
+            string template = GenerateTemplate(resourceProperties);
 
             TemplateAnalyzer templateAnalyzer = new TemplateAnalyzer(template);
             var evaluations = templateAnalyzer.EvaluateRulesAgainstTemplate();
@@ -27,19 +30,28 @@ namespace Microsoft.Azure.Templates.Analyzer.Core.UnitTests
                 Assert.AreEqual(expectedEvaluationPassed, evaluations.First().Passed);
         }
 
-        private string GenerateTemplate(string resourceProperties, string resourceType)
+        private string GenerateTemplate(string[] resourceProperties)
         {
             return string.Format(@"{{
             ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"",
             ""contentVersion"": ""1.0.0.0"",
-            ""resources"": [
+            ""resources"": [ {0} ] }}", string.Join(',', resourceProperties));
+        }
+
+        private string GenerateResource(string resourceProperties, string resourceType)
+        {
+            if (string.IsNullOrEmpty(resourceProperties))
+            {
+                return null;
+            }
+
+            return string.Format(@"
                     {{
                       ""apiVersion"": ""2018-02-01"",
                       ""name"": ""resourceName"",
                       ""type"": ""{1}"",
                       ""properties"": {0}
-                    }}
-            ]}}", resourceProperties, resourceType);
+                    }}", resourceProperties, resourceType);
         }
 
         [TestMethod]
