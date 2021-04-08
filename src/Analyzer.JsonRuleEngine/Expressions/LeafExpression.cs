e3c1bf1c1b149da8e92f3556b1672520d6491f35
Johnathon Mohr <johnmohr@microsoft.com>
2021-04-08 13:50:46 -0700

    Small comment update.

--- src/Analyzer.JsonRuleEngine/Expressions/LeafExpression.cs
+++ src/Analyzer.JsonRuleEngine/Expressions/LeafExpression.cs
@@ -21,7 +21,8 @@ namespace Microsoft.Azure.Templates.Analyzer.RuleEngines.JsonEngine.Expressions
         /// <param name="jsonLineNumberResolver">An <see cref="ILineNumberResolver"/> to
         /// map JSON paths in evaluation results to the line number in the JSON evaluated.</param>
         /// <param name="operator">The operator used to evaluate the resource type and/or path.</param>
-        /// <param name="commonProperties">The properties common across all <see cref="Expression"/> types.</param>
+        /// <param name="commonProperties">The properties common across all <see cref="Expression"/> types.
+        /// <see cref="ExpressionCommonProperties.Path"/> must not be null.</param>
         public LeafExpression(ILineNumberResolver jsonLineNumberResolver, LeafExpressionOperator @operator, ExpressionCommonProperties commonProperties)
             : base(commonProperties)
         {
