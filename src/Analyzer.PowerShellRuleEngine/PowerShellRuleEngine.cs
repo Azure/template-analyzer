1f2366da60265e1e65b76f3c3dbf3c51e59c6cba
Vera Bogdanich Espina <verabo@microsoft.com>
2021-05-19 10:43:35 -0700

    Report arm-ttk warnings too

--- src/Analyzer.PowerShellRuleEngine/PowerShellRuleEngine.cs
+++ src/Analyzer.PowerShellRuleEngine/PowerShellRuleEngine.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Templates.Analyzer.RuleEngines.PowerShellEngine
         /// <param name="templateFullFilePath">The full file path of the template under analysis.</param>
         public static IEnumerable<IEvaluation> EvaluateRules(string templateFullFilePath)
         {
-            var powerShell = System.Management.Automation.PowerShell.Create(); // FIXME namespace
+            var powerShell = System.Management.Automation.PowerShell.Create();
 
             powerShell.Streams.Error.DataAdded += (sender, e) => HandleDataAddedInStreams(powerShell.Streams.Error[e.Index], ConsoleColor.Red);
             powerShell.Streams.Warning.DataAdded += (sender, e) => HandleDataAddedInStreams(powerShell.Streams.Warning[e.Index], ConsoleColor.Yellow);
@@ -46,32 +46,30 @@ namespace Microsoft.Azure.Templates.Analyzer.RuleEngines.PowerShellEngine
 
             foreach (dynamic executionResult in executionResults)
             {
-                if (executionResult != null && !executionResult.Passed) // We only want to report failures for now
+                var evaluationResults = new List<PowerShellRuleResult>();
+                var extraInfo = ""; // Temporal until the JSON engine also reports more info like variable names, warnings
+
+                foreach (dynamic warning in executionResult.Warnings)
                 {
-                    var evaluationResults = new List<PowerShellRuleResult>();
-                    var possibleErrorDescriptions = "";
+                    extraInfo = extraInfo + "Warning: " + warning.ToString() + ". ";
+                }
 
-                    // TODO check warings too?
-                    if (executionResult.Errors.Count > 0)
+                foreach (dynamic error in executionResult.Errors)
+                {
+                    var lineNumber = 0;
+                    if (error.TargetObject is PSObject targetObject && targetObject.Properties["lineNumber"] != null)
                     {
-                        foreach (dynamic error in executionResult.Errors)
-                       {
-                            var lineNumber = 0;
-                            if (error.TargetObject is PSObject targetObject && targetObject.Properties["lineNumber"] != null)
-                            {
-                                lineNumber = error.TargetObject.lineNumber;
-                            }
-
-                            var evaluationResult = new PowerShellRuleResult(executionResult.Passed, lineNumber);
-                            evaluationResults.Add(evaluationResult);
-
-                            possibleErrorDescriptions = possibleErrorDescriptions + error.ToString() + ". "; // Temporal until the JSON engine also reports more info like variable names
-                        }
+                        lineNumber = error.TargetObject.lineNumber;
                     }
 
-                    var evaluation = new PowerShellRuleEvaluation(executionResult.Name, possibleErrorDescriptions, executionResult.Passed, evaluationResults);
-                    evaluations.Add(evaluation);
+                    var evaluationResult = new PowerShellRuleResult(executionResult.Passed, lineNumber);
+                    evaluationResults.Add(evaluationResult);
+
+                    extraInfo = extraInfo + error.ToString() + ". ";
                 }
+                    
+                var evaluation = new PowerShellRuleEvaluation(executionResult.Name, extraInfo, executionResult.Passed, evaluationResults);
+                evaluations.Add(evaluation);
             }
 
             return evaluations;
