085a42bc945d5b2ed7ed8e8a865d56928671de46
Johnathon Mohr <johnmohr@microsoft.com>
2021-06-14 12:52:01 -0700

    New test project for testing built-in JSON rules against bad templates, with tests for 3 rules.

---
+++ src/Analyzer.Core.JsonRuleTests/TestConfiguration.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Templates.Analyzer.Core.JsonRuleTests
+{
+    [JsonObject]
+    public class TestConfiguration
+    {
+        [JsonProperty]
+        public string Template;
+
+        [JsonProperty]
+        public bool ExpectPass;
+
+        [JsonProperty]
+        public int[] ReportedLines;
+
+        public string TestName { get; set; }
+    }
+}
---
+++ src/Analyzer.Core.JsonRuleTests/TestRunner.cs
@@ -0,0 +1,115 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Azure.Templates.Analyzer.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Templates.Analyzer.Core.JsonRuleTests
+{
+    [TestClass]
+    public class TestRunner
+    {
+        Dictionary<string, IEnumerable<IEvaluation>> templateEvaluations = new();
+
+        /// <summary>
+        /// Runs a test defined in a test configuration file in the Tests directory.
+        /// </summary>
+        /// <param name="ruleExpectations">The test configuration to run.</param>
+        [DataTestMethod]
+        [DynamicData(nameof(GetTests), DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetTestDisplayName))]
+        public void TestRule(TestConfiguration ruleExpectations)
+        {
+            // Get template to analyze
+            var testTemplatePath = @$"TestTemplates\{ruleExpectations.Template}.badtemplate";
+            var testTemplate = File.ReadAllText(testTemplatePath);
+
+            // If not already analyzed, analyze it and store evaluations
+            if (!templateEvaluations.TryGetValue(testTemplatePath, out var results))
+            {
+                var templateAnalyzer = new TemplateAnalyzer(testTemplate);
+                results = templateAnalyzer.EvaluateRulesAgainstTemplate();
+                templateEvaluations[testTemplatePath] = results;
+            }
+
+            // Find any instances of the rule being tested
+            var thisRuleEvaluations = results.ToList().Where(e => e.RuleName.Equals(ruleExpectations.TestName, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (thisRuleEvaluations.Count == 0)
+            {
+                // Rule should be expected to pass if no results are returned
+                Assert.IsTrue(ruleExpectations.ExpectPass);
+            }
+
+            foreach (var evaluation in thisRuleEvaluations)
+            {
+                Assert.AreEqual(ruleExpectations.ExpectPass, evaluation.Passed);
+
+                if (!evaluation.Passed)
+                {
+                    // Get all lines reported as failed
+                    var failingLines = GetAllResults(evaluation)
+                        .Where(r => !r.Passed)
+                        .Select(r => r.LineNumber).ToHashSet();
+
+                    // Verify all expected lines are reported
+                    var expectedLines = ruleExpectations.ReportedLines.ToHashSet();
+                    Assert.IsTrue(failingLines.SetEquals(expectedLines),
+                        "Expected failing lines do not match actual failed lines.  " +
+                        $"Expected: [{string.Join(",", expectedLines)}]  Actual: [{string.Join(",", failingLines)}]");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recursively get all <see cref="IResult"/> objects inside an <see cref="IEvaluation"/>.
+        /// </summary>
+        /// <param name="evaluation">The <see cref="IEvaluation"/> to get the <see cref="IResult"/> from.</param>
+        /// <returns>All <see cref="IResult"/>s in the <see cref="IEvaluation"/>.</returns>
+        private IEnumerable<IResult> GetAllResults(IEvaluation evaluation)
+        {
+            foreach (var result in evaluation.Results)
+            {
+                yield return result;
+            }
+            foreach (var subEvaluation in evaluation.Evaluations)
+            {
+                foreach (var subResult in GetAllResults(subEvaluation))
+                {
+                    yield return subResult;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads all test configuration files from the Tests directory and provides them to <see cref="TestRule"/> to run the test.
+        /// </summary>
+        /// <returns>All the test configurations to run tests with.</returns>
+        public static IEnumerable<object[]> GetTests()
+        {
+            var testConfigurationFiles = Directory.GetFiles("Tests");
+            foreach (var testConfigFile in testConfigurationFiles)
+            {
+                var tests = JsonConvert.DeserializeObject<TestConfiguration[]>(File.ReadAllText(testConfigFile));
+                foreach (var test in tests)
+                {
+                    test.TestName = Path.GetFileNameWithoutExtension(testConfigFile);
+                    yield return new object[] { test }; 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates the test display name based on the test configuration used for a test.
+        /// </summary>
+        /// <param name="_">Not used, but input is required in order to match the function signature expected by MSTest.</param>
+        /// <param name="input">The test input given to <see cref="TestRule"/>.</param>
+        /// <returns>The display name of the test.</returns>
+        public static string GetTestDisplayName(MethodInfo _, object[] input) => (input?[0] as TestConfiguration)?.TestName ?? "Unknown";
+    }
+}
