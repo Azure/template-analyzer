f46130fb4cb96b7023d5a537ae6f657c4081c9ce
Vera Bogdanich Espina <verabo@microsoft.com>
2021-06-21 17:00:03 -0700

    Add initial version of the Greater/LessOrEqualsOperator by transforming the Greater/LessOperator into a InequalityOperator

---
+++ src/Analyzer.JsonRuleEngine.UnitTests/InequalityOperatorTests.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Azure.Templates.Analyzer.RuleEngines.JsonEngine.Operators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.Templates.Analyzer.RuleEngines.JsonEngine.UnitTests
+{
+    [TestClass]
+    public class InequalityOperatorTests
+    {
+        [DataTestMethod]
+        // >int
+        [DataRow(2, 1, false, false, true, DisplayName = "An integer is greater than another integer")]
+        [DataRow(1, 2, false, false, false, DisplayName = "An integer is not greater than another integer")]
+        // <int
+        [DataRow(1, 2, true, false, true, DisplayName = "An integer is less than another integer")]
+        [DataRow(2, 1, true, false, false, DisplayName = "An integer is not less than another integer")]
+        // >=int
+        [DataRow(2, 1, false, true, true, DisplayName = "An integer is greater or equal to another integer")]
+        [DataRow(1, 2, false, true, false, DisplayName = "An integer is not greater or equal to another integer")]
+        [DataRow(2, 2, false, true, true, DisplayName = "An integer is greater or equal to another integer because both are equal")]
+        // <=int
+        [DataRow(1, 2, true, true, true, DisplayName = "An integer is less or equal to another integer")]
+        [DataRow(2, 1, true, true, false, DisplayName = "An integer is not less or equal to another integer")]
+        [DataRow(1, 1, true, true, true, DisplayName = "An integer is less or equal to another integer because both are equal")]
+        // >float
+        [DataRow(2.8, 1.3, false, false, true, DisplayName = "A float is greater than another float")]
+        [DataRow(1.3, 2.8, false, false, false, DisplayName = "A float is not greater than another float")]
+        // <float
+        [DataRow(1.3, 2.8, true, false, true, DisplayName = "A float is less than another float")]
+        [DataRow(2.8, 1.3, true, false, false, DisplayName = "A float is not less than another float")]
+        // >=float
+        [DataRow(2.8, 1.3, false, true, true, DisplayName = "A float is greater or equal to another float")]
+        [DataRow(1.3, 2.8, false, true, false, DisplayName = "A float is not greater or equal to another float")]
+        [DataRow(2.8, 2.8, false, true, true, DisplayName = "A float is greater or equal to another float because both are equal")]
+        // <=float
+        [DataRow(1.3, 2.8, true, true, true, DisplayName = "A float is less or equal to another float")]
+        [DataRow(2.8, 1.3, true, true, false, DisplayName = "A float is not less or equal to another float")]
+        [DataRow(1.3, 1.3, true, true, true, DisplayName = "A float is less or equal to another float because both are equal")]
+        public void EvaluateExpression_ValidNumericType_ReturnsExpectedEvaluationResult(object leftValue, object rightValue, bool isNegative, bool andEquals, bool evaluationResult)
+        {
+            CompareObjects(leftValue, rightValue, isNegative, andEquals, evaluationResult);
+        }
+
+        [DataTestMethod]
+        // >date
+        [DataRow(637676928000000000, 637500672000000000, false, false, true, DisplayName = "A date is greater than another date")]
+        [DataRow(637500672000000000, 637676928000000000, false, false, false, DisplayName = "A date is not greater than another date")]
+        // <date
+        [DataRow(637500672000000000, 637676928000000000, true, false, true, DisplayName = "A date is less than another date")]
+        [DataRow(637676928000000000, 637500672000000000, true, false, false, DisplayName = "A date is not less than another date")]
+        // >=date
+        [DataRow(637676928000000000, 637500672000000000, false, true, true, DisplayName = "A date is greater or equal to another date")]
+        [DataRow(637500672000000000, 637676928000000000, false, true, false, DisplayName = "A date is not greater or equal to another date")]
+        [DataRow(637676928000000000, 637676928000000000, false, true, true, DisplayName = "A date is greater or equal to another date because both are equal")]
+        // <=date
+        [DataRow(637500672000000000, 637676928000000000, true, true, true, DisplayName = "A date is less or equal to another date")]
+        [DataRow(637676928000000000, 637500672000000000, true, true, false, DisplayName = "A date is not less or equal to another date")]
+        [DataRow(637500672000000000, 637500672000000000, true, true, true, DisplayName = "A date is less or equal to another date because both are equal")]
+        public void EvaluateExpression_ValidDateType_ReturnsExpectedEvaluationResult(long leftTicks, long rightTicks, bool isNegative, bool andEquals, bool evaluationResult)
+        {
+            var leftDate = new DateTime(leftTicks);
+            var rightDate = new DateTime(rightTicks);
+
+            CompareObjects(leftDate, rightDate, isNegative, andEquals, evaluationResult);
+        }
+
+        [TestMethod]
+        public void GetName_ReturnsCorrectName()
+        {
+            Assert.AreEqual("LessOrEquals", new InequalityOperator(new JObject(), isNegative: true, andEquals: true).Name);
+            Assert.AreEqual("Less", new InequalityOperator(new JObject(), isNegative: true, andEquals: false).Name);
+            Assert.AreEqual("GreaterOrEquals", new InequalityOperator(new JObject(), isNegative: false, andEquals: true).Name);
+            Assert.AreEqual("Greater", new InequalityOperator(new JObject(), isNegative: false, andEquals: false).Name);
+
+        }
+
+        private void CompareObjects(object left, object right, bool isNegative, bool andEquals, bool evaluationResult)
+        {
+            var leftJToken = TestUtilities.ToJToken(left);
+            var rightJToken = TestUtilities.ToJToken(right);
+
+            var greaterOperator = new InequalityOperator(leftJToken, isNegative: isNegative, andEquals: andEquals);
+
+            Assert.AreEqual(evaluationResult, greaterOperator.EvaluateExpression(rightJToken));
+        }
+    }
+}
---
+++ src/Analyzer.JsonRuleEngine/Operators/InequalityOperator.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.Templates.Analyzer.RuleEngines.JsonEngine.Operators
+{
+    /// <summary>
+    /// An operator that evaluates the "greater", "greaterOrEquals", "less", and "lessOrEquals" JSON expressions.
+    /// </summary>
+    internal class InequalityOperator : LeafExpressionOperator
+    {
+        /// <inheritdoc/>
+        public override string Name => GetName();
+
+        /// <summary>
+        /// Whether the operator also considers equality
+        /// </summary>
+        private Boolean andEquals;
+
+        /// <summary>
+        /// Creates an InequalityOperator.
+        /// </summary>
+        /// <param name="specifiedValue">The value specified in the JSON rule.</param>
+        /// <param name="isNegative">Whether the operator compares by greater than or by less than.</param>
+        /// <param name="andEquals">Whether the operator also considers equality.</param>
+        public InequalityOperator(JToken specifiedValue, bool isNegative, bool andEquals)
+        {
+            this.SpecifiedValue = specifiedValue ?? throw new ArgumentNullException(nameof(specifiedValue));
+            this.IsNegative = isNegative;
+            this.andEquals = andEquals;
+        }
+
+        /// <summary>
+        /// Evaluates <paramref name="tokenToEvaluate"/> to determine if it is greater, greaterOrEquals, less, or lessOrEquals than the specified value.
+        /// </summary>
+        /// <param name="tokenToEvaluate">The JToken to evaluate.</param>
+        /// <returns>A value indicating whether or not the evaluation passed.</returns>
+        public override bool EvaluateExpression(JToken tokenToEvaluate)
+        {
+            if (tokenToEvaluate == null)
+            {
+                // If the specified property in the JSON is not defined then we would assume it could potentially have an undesired value
+                return false;
+            }
+
+            // TODO before this catch wrong types, like float vs date
+
+            var normalizedSpecifiedValue = GetNormalizedValue(this.SpecifiedValue);
+            var normalizedTokenToEvaluate = GetNormalizedValue(tokenToEvaluate);
+
+            var result = normalizedSpecifiedValue > normalizedTokenToEvaluate;
+
+            if (this.IsNegative)
+            {
+                result = !result;
+            }
+
+            if (this.andEquals)
+            {
+                result = result || normalizedSpecifiedValue == normalizedTokenToEvaluate;
+
+            }
+
+            return result;
+        }
+
+        private double GetNormalizedValue(JToken token)
+        {
+            double value;
+
+            if (token.Type == JTokenType.Date)
+            {
+                value = token.Value<DateTime>().ToOADate();
+            } else
+            {
+                // TODO catch type errors earlier?
+                value = token.Value<double>();
+            }
+
+            return value;
+        }
+
+        private string GetName() {
+            if (IsNegative && andEquals)
+            {
+                return "LessOrEquals";
+            }
+            else if (IsNegative && !andEquals)
+            {
+                return "Less";
+            }
+            else if (!IsNegative && andEquals)
+            {
+                return "GreaterOrEquals";
+            }
+            else
+            {
+                return "Greater";
+            }
+        }
+    }
+}
