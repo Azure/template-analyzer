6d626a729c267a9ec0bd179ac4bc6a958dab4968
Norbu Sonam <t-norbusonam@microsoft.com>
2021-07-15 15:46:37 -0400

    upgraded bicep

--- src/Analyzer.BicepProcessor/BicepTemplateProcessor.cs
+++ src/Analyzer.BicepProcessor/BicepTemplateProcessor.cs
@@ -5,7 +5,6 @@ using System.IO;
 using Bicep.Core.Emit;
 using Bicep.Core.FileSystem;
 using Bicep.Core.Semantics;
-using Bicep.Core.Syntax;
 using Bicep.Core.TypeSystem.Az;
 using Bicep.Core.Workspaces;
 
@@ -18,18 +17,17 @@ namespace Microsoft.Azure.Templates.Analyzer.BicepProcessor
     {
         /// <summary>
         /// Converts Bicep template into JSON template and returns it as a string
-        /// </summary>/// <param name="bicepPath">The Bicep Template file path.</param>
+        /// </summary>
+        /// <param name="bicepPath">The Bicep Template file path.</param>
         /// <returns>The processed template as a <c>JSON</c> object.</returns>
         static public string ConvertBicepToJson(string bicepPath)
         {
             using var stringWriter = new StringWriter();
-            var syntaxTreeGrouping = SyntaxTreeGroupingBuilder.Build(new FileResolver(), new Workspace(), PathHelper.FilePathToFileUrl(bicepPath));
+            var syntaxTreeGrouping = SourceFileGroupingBuilder.Build(new FileResolver(), new Workspace(), PathHelper.FilePathToFileUrl(bicepPath));
             var compilation = new Compilation(AzResourceTypeProvider.CreateWithAzTypes(), syntaxTreeGrouping);
             var emitter = new TemplateEmitter(compilation.GetEntrypointSemanticModel(), "");
             emitter.Emit(stringWriter);
             return stringWriter.ToString();
         }
-
     }
-
 }
